aa71d80b402e39102e2032c0ce6d7724497e67dd
rapitkan <rapitkan@gmail.com>
2016-04-14 16:51:05 +0300

    Määritelty sovelluken reitit.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'JUKU';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'etusivu'],   name: 'etusivu',        moduleId: 'etusivu',        nav: true, title: 'Etusivu' },
+      { route: 'tilastot',        name: 'tilastot',       moduleId: 'tilastot',       nav: true, title: 'Tilastot' },
+      { route: 'tilastot/valtionavustukset',  name: 'valtionavustukset', moduleId: 'valtionavustukset', nav: false, title: 'Valtionavustukset' },
+      { route: 'tilastot/perustunnusluvut',  name: 'perustunnusluvut', moduleId: 'perustunnusluvut', nav: false, title: 'Perustunnusluvut' },
+      { route: 'tilastot/kaikkiTunnusluvut',  name: 'kaikkiTunnusluvut', moduleId: 'kaikkiTunnusluvut', nav: false, title: 'Kaikki tunnusluvut' },
+      { route: 'kilpailutukset',  name: 'kilpailutukset', moduleId: 'kilpailutukset', nav: true, title: 'Kilpailutukset' }
+
     ]);
 
     this.router = router;
