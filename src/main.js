7e3d108bbd237cbc46ee035d4d5d39e68365e5cb
rapitkan <rapitkan@gmail.com>
2016-04-14 16:49:45 +0300

    Tehty tarvittavat määritykset Polymer-kirjaston ottamiseksi käyttöön ja disabloitu kehitystilan debug-moodi.

--- src/main.js
+++ src/main.js
@@ -2,15 +2,16 @@ import 'bootstrap';
 
 export function configure(aurelia) {
   aurelia.use
-    .standardConfiguration()
-    .developmentLogging();
+    .standardConfiguration();
+    // .developmentLogging();
 
   //Uncomment the line below to enable animation.
-  //aurelia.use.plugin('aurelia-animator-css');
+  aurelia.use.plugin('aurelia-animator-css');
   //if the css animator is enabled, add swap-order="after" to all router-view elements
 
+  aurelia.use.plugin('aurelia-polymer');
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
-  //aurelia.use.plugin('aurelia-html-import-template-loader')
+  aurelia.use.plugin('aurelia-html-import-template-loader')
 
   aurelia.start().then(() => aurelia.setRoot());
 }
