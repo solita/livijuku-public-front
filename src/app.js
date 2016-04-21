7a07252ec8bc26ff4f60ddf8e0574e94273a6d35
rapitkan <rapitkan@gmail.com>
2016-04-21 17:06:54 +0300

    Lisätty tilastosivut reitittimen asetuksiin ja korvattu staattinen navigointi dynaamisella. Poistettu testimielessä ollut 2000px:n sivukorkeus.

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ export class App {
   configureRouter(config, router) {
     config.title = 'JUKU';
     config.map([
-      { route: ['', 'etusivu'],   name: 'etusivu',        moduleId: 'etusivu',        nav: true, title: 'Etusivu' },
-      { route: 'tilastot',        name: 'tilastot',       moduleId: 'tilastot',       nav: true, title: 'Tilastot' },
-      { route: 'tilastot/valtionavustukset',  name: 'valtionavustukset', moduleId: 'valtionavustukset', nav: false, title: 'Valtionavustukset' },
-      { route: 'tilastot/perustunnusluvut',  name: 'perustunnusluvut', moduleId: 'perustunnusluvut', nav: false, title: 'Perustunnusluvut' },
-      { route: 'tilastot/kaikkiTunnusluvut',  name: 'kaikkiTunnusluvut', moduleId: 'kaikkiTunnusluvut', nav: false, title: 'Kaikki tunnusluvut' },
+      { route: ['', 'etusivu'],   name: 'etusivu',  moduleId: 'etusivu',  nav: false, title: 'Etusivu' },
+      { route: 'tilastot',        name: 'tilastot', moduleId: 'tilastot', nav: false,  title: 'Tilastot' },
+      { route: 'tilastot/valtionavustukset',  name: 'valtionavustukset',  moduleId: 'valtionavustukset', nav: true, title: 'Valtionavustukset' },
+      { route: 'tilastot/perustunnusluvut',   name: 'perustunnusluvut',   moduleId: 'perustunnusluvut',  nav: true, title: 'Perustunnusluvut' },
+      { route: 'tilastot/kaikkiTunnusluvut',  name: 'kaikkiTunnusluvut',  moduleId: 'kaikkiTunnusluvut', nav: true, title: 'Kaikki tunnusluvut' },
       { route: 'kilpailutukset',  name: 'kilpailutukset', moduleId: 'kilpailutukset', nav: true, title: 'Kilpailutukset' }
-
     ]);
-
     this.router = router;
   }
 }
