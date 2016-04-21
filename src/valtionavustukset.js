1ef3453ed2e6bd44dc3691c549cf67e2ea1daf0d
rapitkan <rapitkan@gmail.com>
2016-04-21 17:04:41 +0300

    Lisätty navigointi Valtionavustukset-sivun eri graafien välillä.

--- src/valtionavustukset.js
+++ src/valtionavustukset.js
@@ -1,2 +1,21 @@
+import { inject } from 'aurelia-framework';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import {HttpClient} from 'aurelia-http-client';
+import R from 'ramda';
+
+@inject(EventAggregator, HttpClient)
 export class Valtionavustukset {
+
+  constructor(EventAggregator, HttpClient) {
+    this.ea = EventAggregator;
+    this.http = HttpClient;
+    this.childRoute = "ALL";
+  }
+
+  attached() {
+    this.ea.subscribe('router:navigation:success', router => {
+      this.childRoute = router.instruction.params.childRoute;
+    });
+  }
+
 }
