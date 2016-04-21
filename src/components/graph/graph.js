14faa0ecd86e76ea9a0ed69996ec9a803c3c475f
rapitkan <rapitkan@gmail.com>
2016-04-21 17:08:32 +0300

    Muokattu graafikomponenttia, joka sisältää vielä toistaiseksi kovakoodattua dataa.

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -1,8 +1,79 @@
+import {bindable, inject} from 'aurelia-framework';
 import d3 from 'd3';
+import nv from 'nvd3';
 
+@inject(Element)
 export class GraphCustomElement {
 
-  constructor () {
-    console.info(d3);
+  @bindable height = 200;
+
+  constructor(element) {
+    this.element = element;
+    this.d3 = d3;
+    this.nv = nv;
+    console.info(nv, d3);
   }
+
+  bind() {
+    this.nv.addGraph(() => {
+      let chart = this.nv.models.discreteBarChart()
+      .x(function(d) { return d.label; })    //Specify the data accessors.
+      .y(function(d) { return d.value; })
+      .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
+      // .tooltips(false)        //Don't show tooltips
+      .showValues(true)       //...instead, show the bar value right on top of each bar.
+      // .transitionDuration(350)
+      ;
+
+      d3.select('#chart svg').datum(exampleData()).call(chart);
+
+      nv.utils.windowResize(chart.update);
+
+      return chart;
+    });
+
+    //Each bar represents a single discrete quantity.
+    function exampleData() {
+      return  [
+        {
+          key: 'Cumulative Return',
+          values: [
+            {
+              'label': 'A Label',
+              'value': -29.765957771107
+            },
+            {
+              'label': 'B Label',
+              'value': 0
+            },
+            {
+              'label': 'C Label',
+              'value': 32.807804682612
+            },
+            {
+              'label': 'D Label',
+              'value': 196.45946739256
+            },
+            {
+              'label': 'E Label',
+              'value': 0.19434030906893
+            },
+            {
+              'label': 'F Label',
+              'value': -98.079782601442
+            },
+            {
+              'label': 'G Label',
+              'value': -13.925743130903
+            },
+            {
+              'label': 'H Label',
+              'value': -5.1387322875705
+            }
+          ]
+        }
+      ];
+    }
+  }
+
 }
