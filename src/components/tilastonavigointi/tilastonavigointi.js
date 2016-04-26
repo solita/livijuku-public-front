3103b87df176a4385037f3c51fb1803b192d8408
rapitkan <rapitkan@gmail.com>
2016-04-26 09:25:25 +0300

    Lisätty Tilastonavigointi-komponentti navigointiin tilastosivuilla eri toimivalta-alueiden välillä.

---
+++ src/components/tilastonavigointi/tilastonavigointi.js
@@ -0,0 +1,56 @@
+import {bindable, inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {HttpClient} from 'aurelia-http-client';
+import {Router} from 'aurelia-router';
+import R from 'ramda';
+import MobileDetect from 'mobile-detect';
+
+@inject(EventAggregator, HttpClient, Router)
+export class TilastonavigointiCustomElement {
+
+  @bindable page;
+
+  constructor(EventAggregator, HttpClient, router) {
+    this.ea = EventAggregator;
+    this.http = HttpClient;
+    this.router = router;
+    this.md = new MobileDetect(window.navigator.userAgent);
+    this.viranomaiset = [{
+      id: 'ALL',
+      nimi: 'Kaikki'
+    }, {
+      id: 'KS1',
+      nimi: 'Suuret kaupungit'
+    }, {
+      id: 'KS2',
+      nimi: 'Keskisuuret kaupungit'
+    }, {
+      id: 'KS3',
+      nimi: 'Pienet kaupungit'
+    }, {
+      id: 'ELY',
+      nimi: 'ELY-keskukset'
+    }];
+  }
+
+  attached() {
+    this.ea.subscribe('router:navigation:success', router => {
+      if (router.instruction.fragment.indexOf(this.page) !== -1) {
+        this.childRoute = router.instruction.params.childRoute;
+        this.childRouteIndex = this.getChildRouteIndex();
+        if (this.childRouteIndex === -1) {
+          if (this.md.mobile()) {
+            this.router.navigate('tilastot/' + this.page + '/KS1');
+          } else {
+            this.router.navigate('tilastot/' + this.page + '/ALL');
+          }
+        }
+      }
+    });
+  }
+
+  getChildRouteIndex() {
+    return R.findIndex(R.propEq('id', this.childRoute))(this.viranomaiset);
+  }
+
+}
