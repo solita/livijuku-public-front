e9467504541a60b78d8239e378188ccb77e0d425
rapitkan <rapitkan@gmail.com>
2016-05-06 11:08:42 +0300

    Muokattu sovelluksen urleja siten, etteivät graafeja näyttävät sivut ole enää tilastot-polun alla.

--- src/app.js
+++ src/app.js
@@ -3,10 +3,9 @@ export class App {
     config.title = 'JUKU';
     config.map([
       { route: ['', 'etusivu'],   name: 'etusivu',  moduleId: 'etusivu',  nav: false, title: 'Etusivu' },
-      { route: 'tilastot',        name: 'tilastot', moduleId: 'tilastot', nav: false,  title: 'Tilastot' },
-      { route: 'tilastot/valtionavustukset',  name: 'valtionavustukset',  moduleId: 'valtionavustukset', nav: true, title: 'Valtionavustukset' },
-      { route: 'tilastot/perustunnusluvut',   name: 'perustunnusluvut',   moduleId: 'perustunnusluvut',  nav: true, title: 'Perustunnusluvut' },
-      { route: 'tilastot/kaikkitunnusluvut',  name: 'kaikkitunnusluvut',  moduleId: 'kaikkitunnusluvut', nav: true, title: 'Kaikki tunnusluvut' },
+      { route: 'valtionavustukset',  name: 'valtionavustukset',  moduleId: 'valtionavustukset', nav: true, title: 'Valtionavustukset' },
+      { route: 'perustunnusluvut',   name: 'perustunnusluvut',   moduleId: 'perustunnusluvut',  nav: true, title: 'Perustunnusluvut' },
+      { route: 'kaikkitunnusluvut',  name: 'kaikkiTunnusluvut',  moduleId: 'kaikkiTunnusluvut', nav: true, title: 'Kaikki tunnusluvut' },
       { route: 'kilpailutukset',  name: 'kilpailutukset', moduleId: 'kilpailutukset', nav: true, title: 'Kilpailutukset' }
     ]);
     this.router = router;
