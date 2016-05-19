e1b8c05bdbed38e9f4ee8d10fbfb20db29762590
rapitkan <rapitkan@gmail.com>
2016-05-19 10:22:43 +0300

    Muokattu sivuston reititystä ja lisätty siihen yksittäisen kilpailutuksen sivu.

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ export class App {
       { route: ['', 'etusivu'],   name: 'etusivu',  moduleId: 'etusivu',  nav: false, title: 'Etusivu' },
       { route: 'valtionavustukset',  name: 'valtionavustukset',  moduleId: 'valtionavustukset', nav: true, title: 'Valtionavustukset' },
       { route: 'perustunnusluvut',   name: 'perustunnusluvut',   moduleId: 'perustunnusluvut',  nav: true, title: 'Perustunnusluvut' },
-      { route: 'kaikkitunnusluvut',  name: 'kaikkiTunnusluvut',  moduleId: 'kaikkiTunnusluvut', nav: true, title: 'Kaikki tunnusluvut' },
-      { route: 'kilpailutukset',  name: 'kilpailutukset', moduleId: 'kilpailutukset', nav: true, title: 'Kilpailutukset' }
+      { route: 'kaikkiTunnusluvut',  name: 'kaikkiTunnusluvut',  moduleId: 'kaikkiTunnusluvut', nav: true, title: 'Kaikki tunnusluvut' },
+      { route: 'kilpailutukset',  name: 'kilpailutukset', moduleId: 'kilpailutukset', nav: true, title: 'Kilpailutukset' },
+      { route: 'kilpailutukset/:id',  name: 'kilpailutus', moduleId: 'kilpailutus', nav: false, title: 'Kilpailutus' }
     ]);
     this.router = router;
   }
