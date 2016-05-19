bfd7723e6085ae3245df482a558b3778cc2e2c2a
rapitkan <rapitkan@gmail.com>
2016-05-19 10:07:30 +0300

    Poistettu etusivun kontrollerista turhat koodit ja etusivulta kiinteä korkeus, joka sille oli testausmielessä annettu.

--- src/etusivu.js
+++ src/etusivu.js
@@ -1,18 +1,3 @@
 export class Etusivu {
   heading = 'JUKU';
-  firstName = 'John';
-  lastName = 'Doe';
-  previousValue = this.fullName;
-
-  canDeactivate() {
-    if (this.fullName !== this.previousValue) {
-      return confirm('Are you sure you want to leave?');
-    }
-  }
-}
-
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
-  }
 }
