75d6f84a32f8dd1b07b52147da77451367d94b6c
rapitkan <rapitkan@gmail.com>
2016-05-19 09:59:40 +0300

    LIVIJUKU-873 Lisätty sivu yksittäisen kilpailutuksen näyttämiseen.

---
+++ src/kilpailutus.js
@@ -0,0 +1,18 @@
+import {HttpClient} from 'aurelia-fetch-client';
+import {inject} from 'aurelia-framework';
+
+@inject(HttpClient)
+export class Kilpailutus {
+
+  constructor(http) {
+    this.http = http;
+  }
+
+  activate(params) {
+    this.http.fetch('kilpailutus/' + params.id)
+      .then(response => response.json())
+      .then(data => {
+        this.kilpailutus = data;
+      });
+  }
+}
