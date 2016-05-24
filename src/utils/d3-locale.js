41d27ec6ae49d131482d748373f40720cbcce259
rapitkan <rapitkan@gmail.com>
2016-05-24 11:33:13 +0300

    Validoitu d3:n lokalisaatiotiedosto.

--- src/utils/d3-locale.js
+++ src/utils/d3-locale.js
@@ -1,20 +1,18 @@
-'use strict';
-
 import d3 from 'd3';
 
-var d3_locale_fiFI = d3.locale({
-  decimal: ",",
-  thousands: "\xa0",
+let d3LocaleFiFI = d3.locale({
+  decimal: ',',
+  thousands: '\xa0',
   grouping: [3],
-  currency: ["", "\xa0€"],
-  dateTime: "%A, %-d. %Bta %Y klo %X",
-  date: "%-d.%-m.%Y",
-  time: "%H:%M:%S",
-  periods: ["a.m.", "p.m."],
-  days: ["sunnuntai", "maanantai", "tiistai", "keskiviikko", "torstai", "perjantai", "lauantai"],
-  shortDays: ["Su", "Ma", "Ti", "Ke", "To", "Pe", "La"],
-  months: ["tammikuu", "helmikuu", "maaliskuu", "huhtikuu", "toukokuu", "kesäkuu", "heinäkuu", "elokuu", "syyskuu", "lokakuu", "marraskuu", "joulukuu"],
-  shortMonths: ["Tammi", "Helmi", "Maalis", "Huhti", "Touko", "Kesä", "Heinä", "Elo", "Syys", "Loka", "Marras", "Joulu"]
+  currency: ['', '\xa0€'],
+  dateTime: '%A, %-d. %Bta %Y klo %X',
+  date: '%-d.%-m.%Y',
+  time: '%H:%M:%S',
+  periods: ['a.m.', 'p.m.'],
+  days: ['sunnuntai', 'maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai'],
+  shortDays: ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La'],
+  months: ['tammikuu', 'helmikuu', 'maaliskuu', 'huhtikuu', 'toukokuu', 'kesäkuu', 'heinäkuu', 'elokuu', 'syyskuu', 'lokakuu', 'marraskuu', 'joulukuu'],
+  shortMonths: ['Tammi', 'Helmi', 'Maalis', 'Huhti', 'Touko', 'Kesä', 'Heinä', 'Elo', 'Syys', 'Loka', 'Marras', 'Joulu']
 });
 
-d3.format = d3_locale_fiFI.numberFormat;
+d3.format = d3LocaleFiFI.numberFormat;
