3ba9c8bbc59c43f53e9fa8ce6b0a9e71dad830fe
rapitkan <rapitkan@gmail.com>
2016-05-30 16:50:37 +0300

    Poistettu aurelia-cookie käytöstä.

--- src/main.js
+++ src/main.js
@@ -38,7 +38,5 @@ export function configure(aurelia) {
   //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
   aurelia.use.plugin('aurelia-html-import-template-loader');
 
-  aurelia.use.plugin('aurelia-cookie');
-
   aurelia.start().then(() => aurelia.setRoot());
 }
