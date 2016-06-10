ac97e91632176f1c34114ebddf030a40a6725f75
rapitkan <rapitkan@gmail.com>
2016-06-10 17:12:47 +0300

    LIVIJUKU-852 Muokattu etusivun kuva-aluetta.

--- src/modules/etusivu/etusivu.js
+++ src/modules/etusivu/etusivu.js
@@ -1,3 +1,15 @@
+import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
+
+@inject(Router)
 export class Etusivu {
   heading = 'JUKU';
+
+  constructor(router) {
+    this.router = router;
+  }
+
+  toExtranet() {
+    this.router.navigate('http://extranet.liikennevirasto.fi/juku/');
+  }
 }
