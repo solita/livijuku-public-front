c62f7ff2b77d8e2145f08f81e4240dcfd075d0ae
rapitkan <rapitkan@gmail.com>
2016-06-14 14:23:36 +0300

    LIVIJUKU-927 CSV:n latauspainikkeet luotu graafeille ja kilpailutuksille.

---
+++ src/components/csv/csv.js
@@ -0,0 +1,61 @@
+import {bindable} from 'aurelia-framework';
+import R from 'ramda';
+
+export class CsvCustomElement {
+
+  @bindable data;
+  @bindable filename;
+  @bindable columnDelimiter;
+
+  downloadCSV = () => {
+    let convertArrayOfObjectsToCSV = () => {
+      let result;
+      let ctr;
+      let keys;
+      let columnDelimiter;
+      let lineDelimiter;
+      let data = this.data || null;
+      if (R.isNil(data)) {
+        return null;
+      }
+      columnDelimiter = this.columnDelimiter || ',';
+      lineDelimiter = this.lineDelimiter || '\n';
+
+      keys = Object.keys(data[0]);
+
+      result = '';
+      result += keys.join(columnDelimiter);
+      result += lineDelimiter;
+
+      data.forEach(function(item) {
+        ctr = 0;
+        keys.forEach(function(key) {
+          if (ctr > 0) result += columnDelimiter;
+
+          result += item[key];
+          ctr++;
+        });
+        result += lineDelimiter;
+      });
+      return result;
+    };
+
+    let filename;
+    let link;
+    let csv = convertArrayOfObjectsToCSV();
+    if (csv === null) return;
+
+    filename = this.filename || 'export.csv';
+
+    if (!csv.match(/^data:text\/csv/i)) {
+      csv = 'data:text/csv;charset=utf-8,' + csv;
+    }
+    let data = encodeURI(csv);
+
+    link = document.createElement('a');
+    link.setAttribute('href', data);
+    link.setAttribute('download', filename);
+    link.click();
+  };
+
+}
