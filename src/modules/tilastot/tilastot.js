678a3ef923cb295c7feb832beab894a01ca9bb0c
rapitkan <rapitkan@gmail.com>
2016-06-17 14:39:21 +0300

    Yksittäisiä UI-korjauksia testauksessa tehtyjen huomioiden pohjalta.

--- src/modules/tilastot/tilastot.js
+++ src/modules/tilastot/tilastot.js
@@ -9,17 +9,25 @@ export class Tilastot {
   constructor(eventAggregator, i18n) {
     this.ea = eventAggregator;
     this.i18n = i18n;
+    this.isMobile = document.body.clientWidth < 768;
   }
 
   configureRouter(config, router) {
     config.title = 'Tilastot';
-    config.map([
+    let routeifs = this.isMobile ? [
+      { route: ['', 'KS1'], moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS1', nav: true, title: 'suuret-kaupunkiseudut', settings: { viranomainen: 'KS1'} }
+    ] : [
       { route: ['', 'ALL'], moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'ALL', nav: true, title: 'kaikki', settings: { viranomainen: 'ALL'} },
-      { route: 'KS1', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS1', nav: true, title: 'suuret-kaupunkiseudut', settings: { viranomainen: 'KS1'} },
-      { route: 'KS2', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS2', nav: true, title: 'keskisuuret-kaupunkiseudut', settings: { viranomainen: 'KS2'} },
-      { route: 'KS3', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS3', nav: true, title: 'pienet-kaupunkiseudut', settings: { viranomainen: 'KS3'} },
-      { route: 'ELY', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'ELY', nav: true, title: 'ELY-keskukset', settings: { viranomainen: 'ELY'} }
-    ]);
+      { route: 'KS1', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS1', nav: true, title: 'suuret-kaupunkiseudut', settings: { viranomainen: 'KS1'} }
+    ];
+    let routes = R.concat(routeifs, [{
+      route: 'KS2', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS2', nav: true, title: 'keskisuuret-kaupunkiseudut', settings: { viranomainen: 'KS2'}
+    }, {
+      route: 'KS3', moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'KS3', nav: true, title: 'pienet-kaupunkiseudut', settings: { viranomainen: 'KS3'}
+    }, {
+      route: ['ELY'], moduleId: 'modules/tilastot/viranomainen/viranomainen', name: 'ELY', nav: true, title: 'ELY-keskukset', settings: { viranomainen: 'ELY'}
+    }]);
+    config.map(routes);
     this.router = router;
   }
 
@@ -32,7 +40,7 @@ export class Tilastot {
   initialize() {
     this.pageTitle = this.router.parent.currentInstruction.config.title;
     this.toimivaltaalueet = R.map(alue => { return this.i18n.tr(alue.title); }, this.router.navigation);
-    this.fragment = this.router.currentInstruction.fragment || 'ALL';
+    this.fragment = this.router.currentInstruction.fragment || (this.isMobile ? 'KS1' : 'ALL');
     this.childRouteIndex = R.findIndex(R.propEq('relativeHref', this.router.currentInstruction.fragment))(this.router.navigation) || 0;
   }
 
