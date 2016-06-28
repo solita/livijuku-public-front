dfe2d9faa4a04f99389e99eb0cb4567a979731e8
rapitkan <rapitkan@gmail.com>
2016-06-28 11:00:37 +0300

    Initial commit for Aurelia RC.

---
+++ src/resources/components/chart/chart.js
@@ -0,0 +1,162 @@
+import {I18N} from 'aurelia-i18n';
+import {bindable, inject} from 'aurelia-framework';
+import d3 from 'd3';
+import nv from 'nvd3';
+import R from 'ramda';
+
+@inject(Element, I18N)
+export class ChartCustomElement {
+
+  @bindable setup;
+
+  constructor(Element, i18n) {
+    this.element = Element;
+    this.i18n = i18n;
+    this.d3 = d3;
+    this.nv = nv;
+  }
+
+  setupChanged(setup) {
+    if (this.element.getElementsByTagName('svg')[0]) {
+      d3.selectAll(this.element.getElementsByTagName('svg')[0].childNodes).remove();
+    }
+    this.csv = setup.csv;
+    this.draw(R.tail(setup.data), setup.options);
+    this.title = setup.options.title;
+  }
+
+  draw(data, options) {
+    this.height = options.height || 500;
+    if (R.isNil(data[1])) {
+      this.height = 100;
+    }
+    let parseData = () => {
+      let graphData = [];
+      options.groupKeys.forEach((key, index) => {
+        let item = {
+          key: options.groupLabels[index],
+          values: []
+        };
+        let xLabelsMissing = options.xLabels;
+        data.forEach(value => {
+          if (value[0] === key) {
+            xLabelsMissing = R.filter(label => { return label !== value[1]; }, xLabelsMissing);
+            item.values.push({
+              key: item.key,
+              series: key,
+              x: value[1],
+              y: value[options.valueIndex] || null
+            });
+          }
+        });
+        xLabelsMissing.forEach(label => {
+          item.values.push({
+            key: item.label,
+            series: key,
+            x: label,
+            y: null
+          });
+        });
+        graphData.push(item);
+      });
+      return graphData;
+    };
+
+    this.subtitle = options.subtitle ? options.subtitle.text : '';
+
+    if (options.chart && options.chart.type === 'lineWithFocusChart') {
+      nv.addGraph(() => {
+        let chart = nv.models.lineWithFocusChart();
+
+        chart.useVoronoi(false);
+
+        chart.xAxis
+          .axisLabel(options.chart.xAxis.axisLabel)
+          .tickFormat(options.chart.xAxis.tickFormat);
+
+        chart.x2Axis
+          .axisLabel(options.chart.x2Axis.axisLabel)
+          .tickFormat(options.chart.x2Axis.tickFormat);
+
+        chart.yAxis
+          .axisLabel(options.chart.yAxis.axisLabel)
+          .tickFormat(options.chart.yAxis.tickFormat);
+
+        let chartOptions =  R.merge({}, options.chart);
+
+        delete chartOptions.tooltip;
+        delete chartOptions.xAxis;
+        delete chartOptions.x2Axis;
+        delete chartOptions.yAxis;
+        delete chartOptions.x;
+        delete chartOptions.y;
+
+        chart.options(chartOptions);
+
+        chart.noData(this.i18n.tr('ei-dataa-saatavilla'));
+        if (chartOptions.height) {
+          chart.height(chartOptions.height);
+        }
+
+        d3.select(this.element.getElementsByTagName('svg')[0])
+          .datum(parseData())
+          .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
+    } else {
+      nv.addGraph(() => {
+        let chart = nv.models.multiBarChart();
+        if (options.chart.xAxis.axisLabel) {
+          chart.xAxis.axisLabel(options.chart.xAxis.axisLabel);
+        }
+
+        if (options.chart.xAxis.tickFormat) {
+          chart.xAxis.tickFormat(options.chart.xAxis.tickFormat);
+        }
+
+        if (options.chart.yAxis.axisLabel) {
+          chart.yAxis.axisLabel(options.chart.yAxis.axisLabel);
+        }
+
+        if (options.chart.yAxis.tickFormat) {
+          chart.yAxis.tickFormat(options.chart.yAxis.tickFormat);
+        }
+
+        chart.tooltip.valueFormatter(options.chart.tooltip.valueFormatter);
+
+        let chartOptions =  R.merge({}, options.chart);
+
+        delete chartOptions.tooltip;
+        delete chartOptions.xAxis;
+        delete chartOptions.yAxis;
+        delete chartOptions.x;
+        delete chartOptions.y;
+
+        chart.options(chartOptions);
+
+        chart.noData(this.i18n.tr('ei-dataa-saatavilla'));
+
+        chart.controlLabels({
+          grouped: this.i18n.tr('ryhmitelty'),
+          stacked: this.i18n.tr('pinottu')
+        });
+
+        if (chartOptions.height) {
+          this.height = chartOptions.height;
+          chart.height(chartOptions.height);
+        }
+
+        d3.select(this.element.getElementsByTagName('svg')[0])
+          .datum(parseData())
+          .call(chart);
+
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
+    }
+  }
+}
---
+++ src/resources/elements/kilpailutus/kilpailutus.js
@@ -0,0 +1,36 @@
+import {Api} from 'resources/services/api';
+import {HttpClient} from 'aurelia-fetch-client';
+import {inject} from 'aurelia-framework';
+import R from 'ramda';
+import moment from 'moment';
+// require('moment/locale/fi');
+
+@inject(Api, HttpClient)
+export class Kilpailutus {
+
+  constructor(api, http) {
+    this.api = api;
+    this.http = http;
+  }
+
+  activate(params) {
+    return this.api.organisaatiot.then(organisaatiot => {
+      this.organisaatiot = organisaatiot;
+      this.api.getKilpailutus(params.id).then(data => {
+        this.kilpailutus = data;
+        this.kilpailutus.organisaatio = R.find(R.propEq('id', this.kilpailutus.id), this.organisaatiot);
+        this.kilpailutus.julkaisupvm = R.isNil(this.kilpailutus.julkaisupvm) ? '-' : moment(this.kilpailutus.julkaisupvm).format('dd DD.MM.YYYY');
+        this.kilpailutus.tarjouspaattymispvm = R.isNil(this.kilpailutus.tarjouspaattymispvm) ? '-' : moment(this.kilpailutus.tarjouspaattymispvm).format('dd DD.MM.YYYY');
+        this.kilpailutus.hankintapaatospvm = R.isNil(this.kilpailutus.hankintapaatospvm) ? '-' : moment(this.kilpailutus.hankintapaatospvm).format('dd DD.MM.YYYY');
+        this.kilpailutus.liikennointialoituspvm = R.isNil(this.kilpailutus.liikennointialoituspvm) ? '-' : moment(this.kilpailutus.liikennointialoituspvm).format('dd DD.MM.YYYY');
+        this.kilpailutus.liikennointipaattymispvm = R.isNil(this.kilpailutus.liikennointipaattymispvm) ? '-' : moment(this.kilpailutus.liikennointipaattymispvm).format('dd DD.MM.YYYY');
+        this.kilpailutus.hankittuoptiopaattymispvm = R.isNil(this.kilpailutus.hankittuoptiopaattymispvm) ? '-' : moment(this.kilpailutus.hankittuoptiopaattymispvm).format('dd DD.MM.YYYY');
+        this.kilpailutus.optiopaattymispvm = R.isNil(this.kilpailutus.optiopaattymispvm) ? '-' : moment(this.kilpailutus.optiopaattymispvm).format('dd DD.MM.YYYY');
+      });
+    });
+  }
+
+  backToKilpailutuksetView() {
+    history.back();
+  }
+}
