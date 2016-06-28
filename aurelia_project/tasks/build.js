2d1c5d7aece102c85a6931c20d49e044549087fc
rapitkan <rapitkan@gmail.com>
2016-06-28 15:26:08 +0300

    Vulcanize task added for Polymer elements.

--- aurelia_project/tasks/build.js
+++ aurelia_project/tasks/build.js
@@ -1,6 +1,7 @@
 import gulp from 'gulp';
 import transpile from './transpile';
 import processMarkup from './process-markup';
+import vulcanizeElements from './vulcanize';
 import processCSS from './process-css';
 import {build} from 'aurelia-cli';
 import project from '../aurelia.json';
@@ -10,7 +11,8 @@ export default gulp.series(
   gulp.parallel(
     transpile,
     processMarkup,
-    processCSS
+    processCSS,
+    vulcanizeElements
   ),
   writeBundles
 );
